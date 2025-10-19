1dbd89a2e07150669b63890759421be028a0831f
Jang4360 <jooyoon4360@gmail.com>
2025-10-20 01:28:11 +0900

    feat:restore-setting

--- src/main/java/com/test/webtest/global/api/ApiResponse.java
+++ src/main/java/com/test/webtest/global/api/ApiResponse.java
@@ -3,8 +3,6 @@ package com.test.webtest.global.api;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
-import java.util.List;
-
 /**
  * 최상위 래퍼로 success/error 중 하나만 노출
  */
@@ -13,11 +11,9 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse <T>{
     private final ApiSuccess<T> success;
-    private final ApiError error;
 
-    private ApiResponse(ApiSuccess<T> success, ApiError error) {
+    private ApiResponse(ApiSuccess<T> success, Object object) {
         this.success = success;
-        this.error = error;
     }
 
     public static <T> ApiResponse<T> success(String code, String message, T data) {
@@ -25,12 +21,4 @@ public class ApiResponse <T>{
                 .code(code).message(message).data(data).build(),null);
     }
 
-    public static ApiResponse<?> error(String code, String message, List<ApiError.Detail> details) {
-        return new ApiResponse<>(null, ApiError.builder()
-                .code(code).message(message).details(details).build());
-    }
-
-    public static ApiResponse<?> error(String code, String message) {
-        return error(code, message, null);
-    }
 }
