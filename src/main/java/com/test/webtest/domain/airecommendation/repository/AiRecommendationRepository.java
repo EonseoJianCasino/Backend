431998fbb11fe36b459da830ceb6d15cedf99524
sangji <wsjung1218@gmail.com>
2025-10-23 14:03:56 +0900

    feat: db V5 migration

--- src/main/java/com/test/webtest/domain/airecommendation/repository/AiRecommendationRepository.java
+++ src/main/java/com/test/webtest/domain/airecommendation/repository/AiRecommendationRepository.java
@@ -1,11 +1,11 @@
-//package com.test.webtest.domain.airecommendation.repository;
-//
-//import com.test.webtest.domain.airecommendation.entity.AiRecommendationEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//
-//
-//public interface AiRecommendationRepository extends JpaRepository<AiRecommendationEntity, Long>{
-//
-//
-//}
+package com.test.webtest.domain.airecommendation.repository;
+
+import com.test.webtest.domain.airecommendation.entity.AiRecommendationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface AiRecommendationRepository extends JpaRepository<AiRecommendationEntity, Long>{
+
+
+}
